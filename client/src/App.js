3e4961b237e018f407aa435611f76182588dd7fd
vitalyvitmens <otm_@tut.by>
2023-12-03 18:36:22 +0300

    connected Font Awesome

--- client/src/App.js
+++ client/src/App.js
@@ -11,6 +11,14 @@ export const App = () => {
 		<div className="text-center">
 			<div className="text-blue-600 text-7xl mt-10">Контент</div>
 			<div className="circle-red">77</div>
+			<i className="fa fa-address-book"></i>
+			<i className="fa fa-address-book fa-lg"></i>
+			<i className="fa fa-address-book fa-2x"></i>
+			<i className="fa fa-address-book fa-3x"></i>
+			<i className="fa fa-address-book fa-4x"></i>
+			<i className="fa fa-address-book fa-5x"></i>
+			<i className="fa fa-refresh fa-spin fa-fw fa-5x text-blue-800"></i>
+			<i className="fa fa-address-book fa-5x text-red-600"></i>
 		</div>
 	)
 }
