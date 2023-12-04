db38dd48a199fc0a891b688fa0b3c4d71b7eeac3
vitalyvitmens <otm_@tut.by>
2023-12-04 22:24:21 +0300

    add main styled

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Header = () => {
 	return (
-		<div className="flex justify-between fixed top-0 left-0 right-0 text-center w-full h-28 text-xl tracking-[.015em] text-[#211f20] font-['Georgia_serif'] py-5 px-10 bg-[bisque] shadow-[0_7px_10px_#333] z-10">
+		<div className="flex justify-between fixed top-0 left-0 right-0 text-center w-full h-24 text-xl tracking-[.015em] text-[#211f20] font-['Georgia_serif'] py-5 px-10 bg-[bisque] shadow-[0_7px_10px_#333] z-10">
 			Header
 		</div>
 	)
--- client/src/pages/Main.jsx
+++ client/src/pages/Main.jsx
@@ -13,26 +13,28 @@ export const Main = () => {
 	return !products ? (
 		<Loader />
 	) : (
-		<>
-			<div className='flex w-full text-center mt-40 pl-60 text-blue-900 font-semibold text-2xl'>Продукция ООО "ЦЕНТРОСТАЛЬСТРОЙ"</div>
+		<div className='flex flex-col m-auto mt-24 pt-4 w-[80%] bg-[antiquewhite]  shadow-[0_0_15px_black]'>
+			<div className="flex justify-center mb-4 text-blue-900 font-semibold text-2xl">
+				Продукция ООО "ЦЕНТРОСТАЛЬСТРОЙ"
+			</div>
 			{products.map(({ id, title, image, description, price }) => (
 				<ul key={id}>
 					<li className="flex flex-col mb-10 m-auto text-center">
-						<div className="text-blue-800 font-semibold text-2xl py-2">
+						<div className="text-blue-800 font-semibold text-xl py-4">
 							{title}
 						</div>
 						<img
-							className="rounded-[10px] shadow-[-5px_7px_10px_#000] w-[400px] m-auto"
+							className="rounded-[10px] shadow-[-5px_7px_10px_#000] w-[400px] m-auto mb-2"
 							src={image}
 							alt={image}
 						/>
-						<div className="text-green-800 text-xl">{description}</div>
+						<div className="text-green-800 text-lg">{description}</div>
 						<div className="text-red-500 font-bold text-center">
 							{price} руб
 						</div>
 					</li>
 				</ul>
 			))}
-		</>
+		</div>
 	)
 }
