3ed18037a738006b7084f38d74484bc6aabf4f46
vitalyvitmens <otm_@tut.by>
2023-12-07 00:11:49 +0300

    add visible Relax page

--- client/src/pages/Relax.jsx
+++ client/src/pages/Relax.jsx
@@ -1,9 +1,18 @@
-export const Relax = ({ height = 'min-h-screen' }) => {
-	return (
+import { useState } from 'react'
+
+export const Relax = () => {
+	const [isVisible, setIsVisible] = useState(true)
+
+	setTimeout(() => {
+		setIsVisible(false)
+	}, 5000)
+
+	return isVisible ? (
 		<div
-			className={`flex flex-col items-center justify-center m-auto pt-4 w-[80%] ${height}  bg-[antiquewhite] shadow-[0_0_15px_black]`}
+			className={`flex flex-col items-center justify-center text-center m-auto pt-4 px-40 w-[80%] min-h-screen text-5xl text-blue-900 font-semibold bg-[antiquewhite] shadow-[0_0_15px_black]`}
 		>
-			Relax
+			Перед лицом Космоса большинство людских дел выглядят незначительными, даже
+			пустячными
 		</div>
-	)
+	) : null
 }
