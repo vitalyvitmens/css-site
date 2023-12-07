c830fb5c0cd5c8aa3710cc512d494b200fc045ac
vitalyvitmens <otm_@tut.by>
2023-12-07 17:36:14 +0300

    start write logic CalcPage

--- client/src/pages/CalcPage.jsx
+++ client/src/pages/CalcPage.jsx
@@ -1,9 +1,62 @@
-import React from 'react'
+import { Button } from '../components'
 
 export const CalcPage = () => {
 	return (
-		<div className="flex flex-col m-auto mt-20 pt-8 w-[80%] min-h-screen bg-[antiquewhite]  shadow-[0_0_15px_black]">
-			<div>Расчёт</div>
+		<div className="flex flex-col m-auto mt-20 pt-8 w-[80%] min-h-screen bg-[antiquewhite] shadow-[0_0_15px_black]">
+			<div className="mx-auto">
+				<form className="flex flex-col gap-2">
+					<div className="flex flex-row gap-2 mx-auto">
+						<label htmlFor="ramp">Длина ската, мм</label>
+						<input
+							id="ramp"
+							name="ramp"
+							className="px-2 rounded-md"
+							type="number"
+							placeholder="Длина ската, мм"
+						/>
+					</div>
+					<div className="flex flex-row gap-2 mx-auto">
+						<label htmlFor="cornice1">Длина карниза, мм</label>
+						<input
+							id="cornice1"
+							name="cornice1"
+							className="px-2 rounded-md w-[200px]"
+							type="number"
+							placeholder="Длина карниза, мм"
+						/>
+					</div>
+					<div className="border-2 border-black w-[400px] h-[200px]">
+						<div className="text-center pt-20 text-green-900 text-3xl">
+							Скат №1
+						</div>
+					</div>
+					<div className="border-2 -mt-2 border-black w-[400px] h-[200px] text-center text-green-900 text-3xl">
+						<div className="text-center pt-20 text-green-900 text-3xl">
+							Скат №2
+						</div>
+					</div>
+					<div className="flex flex-row gap-2 mx-auto">
+						<label htmlFor="cornice2">Длина карниза, мм</label>
+						<input
+							id="cornice2"
+							name="cornice2"
+							className="px-2 rounded-md w-[200px]"
+							type="number"
+							placeholder="Длина карниза, мм"
+						/>
+					</div>
+				</form>
+			</div>
+			<div className="mx-auto w-[200px] text-center">
+				<Button
+					bgColor="bg-green-800"
+					onClick={() =>
+						alert('Ещё в процессе разработки, наберитесь терпения!')
+					}
+				>
+					Расчитать
+				</Button>
+			</div>
 		</div>
 	)
 }
--- client/src/pages/UsPage.jsx
+++ client/src/pages/UsPage.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const UsPage = () => {
 	return (
-		<div className="flex flex-col m-auto mt-20 pt-8 w-[80%] min-h-screen bg-[antiquewhite]  shadow-[0_0_15px_black]">
+		<div className="flex flex-col m-auto mt-20 pt-8 w-[80%] min-h-screen bg-[antiquewhite] shadow-[0_0_15px_black]">
 			<div>О нас</div>
 		</div>
 	)
