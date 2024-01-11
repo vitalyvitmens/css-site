ce2750f68ef8eebed6c356bf9d23186e51dbf87e
vitalyvitmens <otm_@tut.by>
2024-01-11 17:36:06 +0300

    feat(client\src\components\Print.jsx): добавляет таблицу расчета стоимости

--- client/src/components/Print.jsx
+++ client/src/components/Print.jsx
@@ -1,36 +1,112 @@
-import React, { useRef } from 'react'
+import { useRef } from 'react'
 import { useReactToPrint } from 'react-to-print' // Импортируем хук для печати компонента
 
 // Компонент для отображения данных в виде таблицы
 const DataTable = ({ data }) => {
+	const sumPrice = (arrObj) =>
+		arrObj.reduce((acc, item) => (acc += item.price), 0).toFixed(2)
+
+	const sumNumberUnits = (arrObj) =>
+		arrObj.reduce((acc, item) => (acc += item.numberUnits), 0).toFixed()
+
+	const sumAmount = (arrObj) =>
+		arrObj.reduce((acc, item) => (acc += item.amount), 0).toFixed(2)
+
 	return (
 		<div className="overflow-x-auto">
-			<table className="table w-full">
+			<table className="table w-full  bg-white">
 				<thead>
 					<tr>
-						<th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
 							№<br />
 							п/п
 						</th>
-						<th>Наименование товара</th>
-						<th>Цвет</th>
-						<th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
+							Наименование товара
+						</th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
+							Цвет
+						</th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
 							Ед.
 							<br />
 							изм.
+						</th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
+							Кол-во
+						</th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
+							Длина,
+							<br />
+							мм
+						</th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
+							Кол-во
+							<br />
+							единиц
+						</th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
+							Отпускная
+							<br />
+							цена, руб.
+							<br />
+							коп.
+						</th>
+						<th className="border-2 border-black bg-[rgb(195,247,221)]">
+							Стоимость,
+							<br />
+							руб. коп.
 						</th>
 					</tr>
 				</thead>
 				<tbody>
 					{data.map((item, index) => (
-						<tr key={index}>
-							<th>{index + 1}</th>
-							<td>{item.name}</td>
-							<td>{item.color}</td>
-							<td>{item.unit}</td>
+						<tr key={index} className="border border-black">
+							<th className="px-0.5 text-end border border-black">
+								{index + 1}
+							</th>
+							<td className="px-0.5 text-start border border-black">
+								{item.name}
+							</td>
+							<td className="px-0.5 text-start border border-black">
+								{item.color}
+							</td>
+							<td className="px-0.5 text-end border border-black">
+								{item.unit}
+							</td>
+							<td className="px-0.5 text-end border border-black">
+								{item.amount}
+							</td>
+							<td className="px-0.5 text-end border border-black">
+								{item.length}
+							</td>
+							<td className="px-0.5 text-end border border-black">
+								{item.numberUnits}
+							</td>
+							<td className="px-0.5 text-end border border-black">
+								{item.sellingPrice}
+							</td>
+							<td className="px-0.5 text-end border border-black">
+								{item.price}
+							</td>
 						</tr>
 					))}
 				</tbody>
+				<tfoot className="border-t-2 border-black">
+					<tr>
+						<th></th>
+						<th>ИТОГО:</th>
+						<th></th>
+						<th></th>
+						<th className="px-0.5 text-end">{sumAmount(data, data.amount)}</th>
+						<th></th>
+						<th className="px-0.5 text-end">
+							{sumNumberUnits(data, data.sumNumberUnits)}
+						</th>
+						<th></th>
+						<th className="px-0.5 text-end">{sumPrice(data, data.price)}</th>
+					</tr>
+				</tfoot>
 			</table>
 		</div>
 	)
@@ -51,37 +127,51 @@ export const Print = () => {
 		{
 			name: 'Металлочерепица 0,50*1210*720 RAL 8019МАТ',
 			color: '8019МАТ',
-			unit: 'м.кв.',
+			unit: 'м2',
+			amount: 164.22,
+			length: '2000',
+			numberUnits: 40,
+			sellingPrice: '19.80',
+			price: 3251.56,
 		},
 		{
 			name: 'Планка конька круглого',
 			color: '8019МАТ',
 			unit: 'пог.м',
+			amount: 80,
+			length: '2000',
+			numberUnits: 40,
+			sellingPrice: '19.80',
+			price: 221.76,
 		},
 		{
 			name: 'Поддон деревянный',
 			color: '',
-			unit: 'шт.',
+			unit: 'шт',
+			amount: 1,
+			length: '',
+			numberUnits: 1,
+			sellingPrice: '30.00',
+			price: 30.0,
 		},
 	]
 
 	return (
 		<div className="container mx-auto p-4">
-			<h1 className="text-3xl font-bold">
-				React JS компонент для вывода данных в виде таблицы и сохранения в PDF
-			</h1>
-			<p className="text-xl">
-				Нажмите на кнопку "Сохранить в PDF", чтобы скачать таблицу в формате
-				PDF:
-			</p>
-			<button
-				className="bg-blue-500 text-white p-2 rounded"
-				onClick={handlePrint}
-			>
-				Сохранить в PDF
-			</button>
-			<div ref={tableRef}>
+			<div className="py-4 px-6 bg-white" ref={tableRef}>
 				<DataTable data={data} />
+			</div>
+			<div className="flex justify-between items-center">
+				<p>
+					Нажмите на кнопку "Сохранить в PDF", чтобы скачать Счет-фактуру в
+					формате PDF:
+				</p>
+				<button
+					className="bg-blue-500 text-white my-2 p-2 rounded"
+					onClick={handlePrint}
+				>
+					Сохранить в PDF
+				</button>
 			</div>
 		</div>
 	)
--- client/src/pages/CalcPage.jsx
+++ client/src/pages/CalcPage.jsx
@@ -26,103 +26,105 @@ export const CalcPage = () => {
 	}
 
 	return (
-		<div className="mx-auto pt-28">
-      <Print/>
-			<H2>Расчет металлочерепицы "Супермонтеррей":</H2>
-			<div className="mx-auto">
-				<form className="flex flex-col gap-2">
-					<Select
-						id="color"
-						title="Выберите цвет кровли, RAL"
-						className="px-1"
-						constantsObject={COLORS}
-						stateValue={color}
-						stateFn={setColor}
-					/>
-					<div className="flex flex-row gap-2">
-						<label htmlFor="cornice1">Длина карниза №1, мм</label>
-						<Input
-							id="cornice1"
-							name="cornice1"
-							className="px-2 rounded-md w-[220px]"
-							placeholder="Длина карниза №1, мм"
-							onChange={(e) => setCornice1Length(Number(e.target.value))}
+		<>
+			<div className="mx-auto pt-28">
+				<H2>Расчет металлочерепицы "Супермонтеррей":</H2>
+				<div className="mx-auto">
+					<form className="flex flex-col gap-2">
+						<Select
+							id="color"
+							title="Выберите цвет кровли, RAL"
+							className="px-1"
+							constantsObject={COLORS}
+							stateValue={color}
+							stateFn={setColor}
 						/>
-					</div>
-					<div
-						className={`flex flex-col justify-center text-center relative border-4 border-double border-gray-200 text-gray-200`}
-						style={{
-							width:
-								cornice1Length > 14000
-									? cornice1Length / 40
-									: cornice1Length / 20,
-							height:
-								cornice1Length > 14000 ? ramp1Length / 40 : ramp1Length / 20,
-							backgroundColor: COLORS[color],
-						}}
-					>
-						<div className="flex flex-col">
-							<Icon
-								id="flex items-center justify-center fa-long-arrow-up text-6xl"
-								color="text-gray-200"
-							>
-								<Input
-									id="ramp1"
-									name="ramp1"
-									className="px-2 rounded-md w-[140px] text-black text-sm mt-2"
-									placeholder="Длина ската №1, мм"
-									onChange={(e) => setRamp1Length(Number(e.target.value))}
-								/>
-							</Icon>
-							Скат №1
+						<div className="flex flex-row gap-2">
+							<label htmlFor="cornice1">Длина карниза №1, мм</label>
+							<Input
+								id="cornice1"
+								name="cornice1"
+								className="px-2 rounded-md w-[220px]"
+								placeholder="Длина карниза №1, мм"
+								onChange={(e) => setCornice1Length(Number(e.target.value))}
+							/>
 						</div>
-					</div>
-					<div
-						className={`flex flex-col justify-center text-center relative border-4 border-double border-gray-200 text-gray-200 -mt-2`}
-						style={{
-							width:
-								cornice2Length > 14000
-									? cornice2Length / 40
-									: cornice2Length / 20,
-							height:
-								cornice2Length > 14000 ? ramp2Length / 40 : ramp2Length / 20,
-							backgroundColor: COLORS[color],
-						}}
-					>
-						<div className="flex flex-col">
-							Скат №2
-							<Icon
-								id="flex items-center justify-center fa-long-arrow-down text-6xl"
-								color="text-gray-200"
-							>
-								<Input
-									id="ramp2"
-									name="ramp2"
-									className="px-2 rounded-md w-[140px] text-black text-sm mb-2"
-									placeholder="Длина ската №2, мм"
-									onChange={(e) => setRamp2Length(Number(e.target.value))}
-								/>
-							</Icon>
+						<div
+							className={`flex flex-col justify-center text-center relative border-4 border-double border-gray-200 text-gray-200`}
+							style={{
+								width:
+									cornice1Length > 14000
+										? cornice1Length / 40
+										: cornice1Length / 20,
+								height:
+									cornice1Length > 14000 ? ramp1Length / 40 : ramp1Length / 20,
+								backgroundColor: COLORS[color],
+							}}
+						>
+							<div className="flex flex-col">
+								<Icon
+									id="flex items-center justify-center fa-long-arrow-up text-6xl"
+									color="text-gray-200"
+								>
+									<Input
+										id="ramp1"
+										name="ramp1"
+										className="px-2 rounded-md w-[140px] text-black text-sm mt-2"
+										placeholder="Длина ската №1, мм"
+										onChange={(e) => setRamp1Length(Number(e.target.value))}
+									/>
+								</Icon>
+								Скат №1
+							</div>
 						</div>
-					</div>
-					<div className="flex flex-row gap-2">
-						<label htmlFor="cornice2">Длина карниза №2, мм</label>
-						<Input
-							id="cornice2"
-							name="cornice2"
-							className="px-2 rounded-md w-[220px]"
-							placeholder="Длина карниза №2, мм"
-							onChange={(e) => setCornice2Length(Number(e.target.value))}
-						/>
-					</div>
-				</form>
+						<div
+							className={`flex flex-col justify-center text-center relative border-4 border-double border-gray-200 text-gray-200 -mt-2`}
+							style={{
+								width:
+									cornice2Length > 14000
+										? cornice2Length / 40
+										: cornice2Length / 20,
+								height:
+									cornice2Length > 14000 ? ramp2Length / 40 : ramp2Length / 20,
+								backgroundColor: COLORS[color],
+							}}
+						>
+							<div className="flex flex-col">
+								Скат №2
+								<Icon
+									id="flex items-center justify-center fa-long-arrow-down text-6xl"
+									color="text-gray-200"
+								>
+									<Input
+										id="ramp2"
+										name="ramp2"
+										className="px-2 rounded-md w-[140px] text-black text-sm mb-2"
+										placeholder="Длина ската №2, мм"
+										onChange={(e) => setRamp2Length(Number(e.target.value))}
+									/>
+								</Icon>
+							</div>
+						</div>
+						<div className="flex flex-row gap-2">
+							<label htmlFor="cornice2">Длина карниза №2, мм</label>
+							<Input
+								id="cornice2"
+								name="cornice2"
+								className="px-2 rounded-md w-[220px]"
+								placeholder="Длина карниза №2, мм"
+								onChange={(e) => setCornice2Length(Number(e.target.value))}
+							/>
+						</div>
+					</form>
+				</div>
+				<div>{calcMetalTiles()}</div>
+				<div className="mx-auto w-[200px] text-center">
+					<Button bgColor="bg-green-800" onClick={calcMetalTiles}>
+						Расчитать
+					</Button>
+				</div>
 			</div>
-			<div>{calcMetalTiles()}</div>
-			<div className="mx-auto w-[200px] text-center">
-				<Button bgColor="bg-green-800" onClick={calcMetalTiles}>
-					Расчитать
-				</Button>
-			</div>
-		</div>
+			<Print />
+		</>
 	)
 }
