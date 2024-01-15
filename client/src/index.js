f1e715a502bd2386a94209fdcfc6764cf81de33e
vitalyvitmens <otm_@tut.by>
2024-01-15 14:30:37 +0300

    chore(client\src\index.js): меняет порядок импортов css & App для исключения перетирания стилей

--- client/src/index.js
+++ client/src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './redux'
-import { App } from './App'
 import './index.css'
+import { App } from './App'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
